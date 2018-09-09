2c3d2584820689c81e8eb976554f4e8c08a0c4ab
Celyn Susanto <celyn.vy@gmail.com>
2018-09-09 19:41:19 +0700

    week3

--- Exercise 13 (Auto) Target Terdekat.js
+++ Exercise 13 (Auto) Target Terdekat.js
@@ -1,14 +1,20 @@
 function targetTerdekat(arr) {
+    //cari o
     var o = arr.indexOf('o');
-    var jarak=0
-    if(arr.indexOf('x')===-1){
-        return 0
-    }
-    for (i = 0; i < arr.lenght; i++) {
+    var temp = arr.length
+    //jika tidak ada x 
+    if (arr.indexOf('x') === -1){
+        return 0}
+    for (i = 0; i < arr.length; i++) {
+        //cari x
         if (arr[i] === 'x') {
-             jarak = Math.abs(o-i)
-    }return jarak
-    } 
+            //jika ketemu x
+            var jarak = Math.abs(o - i);
+            if (jarak < temp) {
+                temp = jarak
+            }
+        }
+    } return temp
 }
 console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x']));
 console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
---
+++ Exercise 14 Mengelompokkan Angka.js
@@ -0,0 +1,37 @@
+//1. Buat array genap
+//2. Buat array ganjil
+//3. Buat array kelipatanTiga
+//4. Buat array tempat
+//5. Lakukan perulangan pada arr
+//6. Buat kondisi jika angka habis dibagi 2 sama dengan 0 masukkan ke array genap
+//7. Buat kondisi jika angka modulus 2 tidak sama dengan 0 masukkan ke array ganjil
+//8. Buat kondisi jika angka modulus 3 sama dengan 0 maka masukkan ke array kelipatanTiga
+//9. Masukkan array genap, array ganjil, dan array kelipatanTiga kedalam tempat
+//8. Kembalikan array tempat
+//9. Selesai
+
+function mengelompokkanAngka(arr) {
+    var arrGenap = []
+    var arrGanjil = []
+    var arrKelTiga = []
+
+
+    for (i = 0; i <= arr.length - 1; i++) {
+        if (arr[i] % 2 == 0 && arr[i] % 3 != 0) {
+            arrGenap.push(arr[i])
+        } else if (arr[i] % 2 != 0 && arr[i] % 3 != 0) {
+            arrGanjil.push(arr[i])
+        } else if (arr[i] % 3 == 0) {
+            arrKelTiga.push(arr[i])
+        } else {
+
+        }
+    } var arrTemp = [arrGenap, arrGanjil, arrKelTiga]
+
+    return arrTemp
+}
+
+console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
+console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
+console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
+console.log(mengelompokkanAngka([])); // [ [], [], [] ]
